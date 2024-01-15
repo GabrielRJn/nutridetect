a38264788317c7ca5a6e34c48e0a936b5fcb7ee7
GabrielRJn <gabrielraianjohnson@gmail.com>
2024-01-15 02:36:45 +0000

    finished about-me

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -1,31 +1,42 @@
-import { Box, ChakraProvider, Heading, Text} from '@chakra-ui/react'
-import React from 'react'
-
-import aboutMeImage from "../../assets/about-me.jpg";
+import React from 'react';
+import { Box, ChakraProvider, Heading, Text, Flex } from '@chakra-ui/react';
+import AboutMeImage from './frontend/assets/about-me.jpg';
+import Header from './frontend/components/Header/Header';
+import Footer from './frontend/components/Footer';
 
 function AboutPage() {
   return (
     <ChakraProvider>
-   < Box
-    position="relative"
-    backgroundImage={`url(${aboutMeImage})`}
-    backgroundSize="cover"
-    backgroundPosition="center"
-    width="100%"
-    height="100%"
-   >
-    <Heading>
-        About this project
-    </Heading>
-
-    <Text>
-        This is a 3rd year project for Anglia Ruskin University.
-        The project is a web application that allows users to upload an image of a food item and get back nutritional information about that food item.
-    </Text>
-    Image by <a href="https://www.freepik.com/free-vector/flat-colorful-university-concept-background_4715899.htm">Freepik</a>
-    </Box>
+      <Flex direction="column" height="100vh">
+        <Header />
+        <Flex flex="1">
+          <Box
+            flex="1"
+            bg="rgba(255, 255, 255, 0.95)"
+            p={8}
+            textAlign="left"
+          >
+            <Heading size="3xl" mb={6} mt={12} ml={5}>About this project</Heading>
+            <Text p={5}  mt={2} fontSize="xl">
+              This is a 3rd-year project for Anglia Ruskin University. The project is a web
+              application that allows users to upload an image of a food item and get back
+              nutritional information about that food item.
+            </Text>
+            <Text mt={2} p={5}>
+              Image by{' '}
+              <a href="https://www.freepik.com/free-vector/flat-colorful-university-concept-background_4715899.htm">
+                Freepik
+              </a>
+            </Text>
+          </Box>
+          <Box flex="1" pl={8} mt={50}>
+            <img src={AboutMeImage} alt="About Me" style={{ width: '100%', height:"50vh" ,borderRadius: '8px' }} />
+          </Box>
+        </Flex>
+        <Footer />
+      </Flex>
     </ChakraProvider>
-  )
+  );
 }
 
-export default AboutPage
+export default AboutPage;
---
+++ src/frontend/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Box, ChakraProvider, Text } from '@chakra-ui/react';
+
+function Footer() {
+  return (
+    <Box
+    bg="blackAlpha.900"
+      color="white"
+      p={4}
+      textAlign="center"
+        position="absolute"
+      bottom="0"
+      height={12}
+      width="100%"
+    >
+      <Text>&copy; NutriDetect | LinkedIn | GitHub</Text> 
+    </Box>
+  );
+}
+
+export default Footer;
